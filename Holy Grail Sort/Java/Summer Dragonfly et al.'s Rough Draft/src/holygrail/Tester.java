34f3fd8fad1d8a86905855f4ebdbfa9e0709bade
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-10-31 12:53:51 -0500

    Move to inner classes

--- Holy Grail Sort/Java/Summer Dragonfly et al.'s Rough Draft/src/holygrail/Tester.java
+++ Holy Grail Sort/Java/Summer Dragonfly et al.'s Rough Draft/src/holygrail/Tester.java
@@ -2,39 +2,39 @@ package holygrail;
 
 import java.util.Comparator;
 
-interface IntegerPair {
-    public Integer getKey();
-    public Integer getValue();
-}
-
-class GrailPair implements IntegerPair {
-    private Integer key;
-    private Integer value;
-    
-    public GrailPair(Integer key, Integer value) {
-        this.key = key;
-        this.value = value;
-    }
-    @Override
-    public Integer getKey() {
-        return this.key;
-    }
-    @Override
-    public Integer getValue() {
-        return this.value;
-    }
-}
-
-class GrailComparator implements Comparator<GrailPair> {
-    @Override
-    public int compare(GrailPair o1, GrailPair o2) {
-        if     (o1.getKey() < o2.getKey()) return -1;
-        else if(o1.getKey() > o2.getKey()) return  1;
-        else                               return  0;
-    }
-}
-
 public class Tester {
+    interface IntegerPair {
+        public Integer getKey();
+        public Integer getValue();
+    }
+
+    class GrailPair implements IntegerPair {
+        private Integer key;
+        private Integer value;
+
+        public GrailPair(Integer key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+        @Override
+        public Integer getKey() {
+            return this.key;
+        }
+        @Override
+        public Integer getValue() {
+            return this.value;
+        }
+    }
+
+    class GrailComparator implements Comparator<GrailPair> {
+        @Override
+        public int compare(GrailPair o1, GrailPair o2) {
+            if     (o1.getKey() < o2.getKey()) return -1;
+            else if(o1.getKey() > o2.getKey()) return  1;
+            else                               return  0;
+        }
+    }
+
     public Tester() {
         new HolyGrailSort<GrailPair>(new GrailComparator());
     }
