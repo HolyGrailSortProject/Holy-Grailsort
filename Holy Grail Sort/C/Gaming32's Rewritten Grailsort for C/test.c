25a1366abc2229125daf0e69fad45388dd5c70e0
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-11-01 16:48:05 -0500

    Update testing variables

--- Holy Grail Sort/C/Gaming32's Rewritten Grailsort for C/test.c
+++ Holy Grail Sort/C/Gaming32's Rewritten Grailsort for C/test.c
@@ -15,7 +15,8 @@
 #define ARRAY_LENGTH 128
 
 // #define RANDOM_LIMIT RAND_MAX
-#define RANDOM_LIMIT 3
+#define RANDOM_LIMIT ARRAY_LENGTH
+// #define RANDOM_LIMIT 3
 
 #define RANDOM_SEED time(NULL)
 
